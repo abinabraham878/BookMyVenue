74592d7d5b797a278dd2ddd6a9a36b2836845b9e
Abin Abraham <abinabraham.work@gmail.com>
2026-05-31 15:06:24 +0530

    feat(user): added enums

---
+++ backend/src/main/java/com/bookmyvenue/backend/user/enums/Role.java
@@ -0,0 +1,7 @@
+package com.bookmyvenue.backend.user.enums;
+
+public enum Role {
+    USER,
+    VENUE_OWNER,
+    ADMIN
+}
---
+++ backend/src/main/java/com/bookmyvenue/backend/user/enums/UserStatus.java
@@ -0,0 +1,7 @@
+package com.bookmyvenue.backend.user.enums;
+
+public enum UserStatus {
+    PENDING_VERIFICATION,
+    ACTIVE,
+    SUSPENDED
+}
