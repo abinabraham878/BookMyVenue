f500d074268ee5553667a6a35e3c62f1e6159f24
Abin Abraham <abinabraham.work@gmail.com>
2026-05-31 15:00:35 +0530

    Chor (User Feature): Created base entity to inherit into child entities

---
+++ backend/src/main/java/com/bookmyvenue/backend/common/entity/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.bookmyvenue.backend.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(
+            name = "created_at",
+            nullable = false,
+            updatable = false,
+            insertable = false
+    )
+    private Instant createdAt;
+
+    @Column(name = "created_by")
+    private UUID createdBy;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "updated_by")
+    private UUID updatedBy;
+
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
+
+    @Column(name = "deleted_by")
+    private UUID deletedBy;
+}
