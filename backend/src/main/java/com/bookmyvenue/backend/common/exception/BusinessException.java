3a198e28d5ea190f7611ba8b88c37d751ff0c4ad
Abin Abraham <abinabraham.work@gmail.com>
2026-06-13 10:40:30 +0530

    feat(user): Exception logic correction

--- backend/src/main/java/com/bookmyvenue/backend/common/exception/BusinessException.java
+++ backend/src/main/java/com/bookmyvenue/backend/common/exception/BusinessException.java
@@ -4,11 +4,11 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class BusinessException extends RuntimeException{
+public class BusinessException extends RuntimeException {
 
     private final ErrorCode errorCode;
 
-    public BusinessException(HttpStatus httpStatus, ErrorCode errorCode){
+    public BusinessException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
